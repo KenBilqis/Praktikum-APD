d71cf0651856ce93fc220a18fbdaebf8cf74bb2a
KenBilqis <ken.bilqis@gmail.com>
2024-09-24 12:17:46 +0700

    add posttest3

--- Post-Test/Post-Test-3/2409106015_KenBilqisNuraini_POSTTEST3.py
+++ Post-Test/Post-Test-3/2409106015_KenBilqisNuraini_POSTTEST3.py
@@ -17,48 +17,104 @@ Silahkan pilih jenis kelamin anda
 2. Wanita''')
 jenis_kelamin = int(input("[1/2]: "))
 
-# user menginput berat dan tinggi badan
-bb = float(input("\nBerat Badan dalam satuan gram: "))
-tb = float(input("Tinggi Badan dalam satuan kilometer: "))
-# mengkonversi satuan menjadi satuan standar
-bb_kg = bb / 1000
-tb_cm = tb * 100000
-
-# user memilihi level aktivitas harian
-print('''
+# pengkondisian jenis kelamin untuk menghitung bmr dan tdee
+if jenis_kelamin == 1:
+    pilihan_jenis_kelamin = "Pria"
+    # user menginput berat dan tinggi badan
+    bb = float(input("\nBerat Badan dalam satuan gram: "))
+    tb = float(input("Tinggi Badan dalam satuan kilometer: "))
+    # mengkonversi satuan menjadi satuan standar
+    bb_kg = bb / 1000
+    tb_cm = tb * 100000
+    bmr = (10 * bb_kg) + (6.25 * tb_cm) - (5 * usia) + 5
+    # user memilihi level aktivitas harian
+    print('''
 Pilih Level Ativitas Harian:
 1. Aktivitas Minimal => jarang bergerak
 2. Aktivitas Sedang => olahraga 1-3 kali seminggu
 3. Aktivitas Tinggi => olahraga 4-7 kali seminggu''')
-level_aktivitas_harian = int(input("[1/2/3]: "))
-
-# pengkondisian level aktivitas harian
-if level_aktivitas_harian == 1:
-    poin_aktivitas_harian = 1.25
-elif level_aktivitas_harian == 2:
-    poin_aktivitas_harian = 1.36
-elif level_aktivitas_harian == 3:
-    poin_aktivitas_harian = 1.72
-else:
-    print("Invalid")
-    exit()
-
-# pengkondisian jenis kelamin untuk menghitung bmr dan tdee
-if jenis_kelamin == 1:
-    pilihan_jenis_kelamin = "Pria"
-    bmr = (10 * bb_kg) + (6.25 * tb_cm) - (5 * usia) + 5
-    tdee = bmr * poin_aktivitas_harian
-elif jenis_kelamin == 2:
-    pilihan_jenis_kelamin = "Wanita"
-    bmr = (10 * bb_kg) + (6.25 * tb_cm) - (5 * usia) - 161
-    tdee = bmr * poin_aktivitas_harian
-else:
-    print("Invalid")
-    exit()
-
-# output program
-print(f'''
+    level_aktivitas_harian = int(input("[1/2/3]: "))
+    # pengkondisian level aktivitas harian
+    if level_aktivitas_harian == 1:
+        poin_aktivitas_harian = 1.25
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
 {nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
 Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
 Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
-sehingga kalori yang dibutuhkan dalam sehari adalah {tdee} kalori''')
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    elif level_aktivitas_harian == 2:
+        poin_aktivitas_harian = 1.36
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
+{nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
+Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
+Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    elif level_aktivitas_harian == 3:
+        poin_aktivitas_harian = 1.72
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
+{nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
+Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
+Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    else:
+        print("Invalid")
+        exit()
+        
+elif jenis_kelamin == 2:
+    pilihan_jenis_kelamin = "Wanita"
+    # user menginput berat dan tinggi badan
+    bb = float(input("\nBerat Badan dalam satuan gram: "))
+    tb = float(input("Tinggi Badan dalam satuan kilometer: "))
+    # mengkonversi satuan menjadi satuan standar
+    bb_kg = bb / 1000
+    tb_cm = tb * 100000
+    bmr = (10 * bb_kg) + (6.25 * tb_cm) - (5 * usia) - 161
+    
+    # user memilihi level aktivitas harian
+    print('''
+Pilih Level Ativitas Harian:
+1. Aktivitas Minimal => jarang bergerak
+2. Aktivitas Sedang => olahraga 1-3 kali seminggu
+3. Aktivitas Tinggi => olahraga 4-7 kali seminggu''')
+    level_aktivitas_harian = int(input("[1/2/3]: "))
+    
+    # pengkondisian level aktivitas harian
+    if level_aktivitas_harian == 1:
+        poin_aktivitas_harian = 1.25
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
+{nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
+Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
+Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    elif level_aktivitas_harian == 2:
+        poin_aktivitas_harian = 1.36
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
+{nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
+Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
+Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    elif level_aktivitas_harian == 3:
+        poin_aktivitas_harian = 1.72
+        tdee = bmr * poin_aktivitas_harian
+        # output program
+        print(f'''
+{nama_user} berusia {usia} berjenis kelamin {pilihan_jenis_kelamin},
+Memiliki berat badan {bb_kg} kg dan tinggi badan {tb_cm} cm.
+Dengan perhitungan BMR {bmr} dan poin level aktivitas harian {poin_aktivitas_harian},
+sehingga kalori yang dibutuhkan dalam sehari adalah {tdee:.0f} kalori''')
+    else:
+        print("Invalid")
+        exit()
+else:
+    print("Invalid")
+    exit()
